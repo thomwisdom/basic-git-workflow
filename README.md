5bee2ff729b753bd81a7bc93ea3d85f598b33e48
Tom Wisdom <thomwisdom@gmail.com>
2020-05-24 18:41:10 -0500

    Add all files since v1.1 release

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This repo will demonstrate a basic git workflow for individuals or small teams
+This line was added locally and hopefully will help cause a merge conflict
--- script.js
+++
@@ -1,7 +0,0 @@
-// Wait for window to load
-document.addEventListener("DOMContentLoaded", function(event) {   // Get reference to header object
-  let myHeader = document.getElementById("header-id")  // Wait 3 seconds, then display the header
-  setTimeout(() => {
-    myHeader.classList.remove("hidden");
-  }, 3000)
-});
